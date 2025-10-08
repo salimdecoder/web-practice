e69e0c8776ca06e7a1d8d2e351f0b689f012b81e
salimdecoder <160842084+salimdecoder@users.noreply.github.com>
2025-10-08 17:56:27 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+function showMessage() {
+  alert("Hi Salim! 🌍 Thanks for visiting my GitHub Page!");
+  document.querySelector("p").innerText = "You clicked the button — JavaScript is working!";
+}
